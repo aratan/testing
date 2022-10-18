ee9a31e31560f99014bf6d48225e691f056ee5ce
aratan <victor.arbiol@gmail.com>
2022-10-18 18:08:47 +0200

    testing

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from fastapi import FastAPI
 
 app = FastAPI()
 
+# activamos las rutas
 @app.get("/")
 async def read_main():
-    return {"msg": "Hello World"}
+    return {"msg": "Hello World ROOT"}
+
+@app.post("/test")
+async def read_main():
+    return {"msg": "Hello World Test"}
 
 client = TestClient(app)
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+
+def test():
+    response = client.post("/test")
+    assert response.status_code == 200
--- test_main.py
+++ test_main.py
@@ -2,9 +2,17 @@ from fastapi.testclient import TestClient
 
 from main import app
 
+
+
+
+
+
 client = TestClient(app)
 
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+
+def test():
+    response = client.post("/test")
+    assert response.status_code == 200
